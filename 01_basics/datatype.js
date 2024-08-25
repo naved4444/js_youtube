d6095684576729f062b7b7cf141e218deba07da9
naved4444 <144813329+naved4444@users.noreply.github.com>
2024-08-25 08:48:14 +0000

    datatypes complete

--- 01_basics/datatype.js
+++ 01_basics/datatype.js
@@ -9,21 +9,21 @@
 // symbol => to identify if a component is unique or not (mostly used in reactJS)
 // object
 
-// console.log(typeof null);                   // O/P : object
-// console.log(typeof undefined);              // O/P : undefined
+console.log(typeof null);                   // O/P : object
+console.log(typeof undefined);              // O/P : undefined
 
 // //                                          CONVERSIONS                                                                 //
 
-// let score = "33";
-// console.log(typeof score);
+let score = "33";
+console.log(typeof score);
 
-// let valueInNumber = Number(score);
-// console.log(typeof valueInNumber);
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
 
-// score = "33abc";
-// valueInNumber = Number(score);
-// console.log(typeof valueInNumber);           // O/P: number
-// console.log(valueInNumber);                 // O/P: NaN (not a number)
+score = "33abc";
+valueInNumber = Number(score);
+console.log(typeof valueInNumber);           // O/P: number
+console.log(valueInNumber);                 // O/P: NaN (not a number)
 
 // let score = null;
 // let valueInNumber = Number(score);
@@ -42,5 +42,20 @@
 let value = 3;
 let valueInNeg = -value;
 console.log(valueInNeg);                            // o/p: -3
+
+console.log(1+4+"2"+3);                                // o/p: 523
+
+console.log(true);                                  // o/p: true
+console.log(+true);                                 // o/p: 1
+// console.log(true+);                              // o/p: error
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2+2;
+console.log(num1);
+
+let str = "02";
+let str_to_num = Number(str);
+console.log(str_to_num);                            // o/p: 2
+console.log(typeof(str_to_num));                    // o/p: number
 
 
